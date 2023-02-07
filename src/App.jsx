9976ad9db13b4b5b693a0c3a0b89c6385d952283
jose antonio <jsegmel@gmail.com>
2023-02-07 16:55:39 +0100

    funciona añadir y eliminar

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,62 @@
 
-import reactLogo from './assets/react.svg'
+
 import './App.css'
 import FullCalendar from '@fullcalendar/react' 
 import dayGridPlugin  from '@fullcalendar/daygrid' 
 import timegridPlugin from '@fullcalendar/timegrid'
 import iterationPluging  from '@fullcalendar/interaction'
 import { useState } from 'react'
-import { Calendar,formatDate } from '@fullcalendar/core'
-import DatePiker from "react-datepicker"
 
 function App() {
-  const events=[{title: "", date: "" }]
-  const[newEvent,setEvent]=useState({title: "", date: "" })
-  const[allEvents,setAllEvents]=useState([])
-  
+  const [eventsData, setEventsData] = useState([{ title: "event 1", date: "2023-02-02" }]);
+ console.log(eventsData)
+  const handleClick=(e)=>{
+    if(confirm(`Desea eliminar el evento${e.event.title}`)){
+      e.event.remove()
+    }
     
-   
-  function handleAddEvent(arg) {
-   setEvent({title:"evento", date:arg.date})
-   setAllEvents(allEvents+newEvent)
-   console.log(allEvents)
+     
   }
   
+  const handleSelect = ({ start, end }) => {
+    const title = window.prompt("Nuevo evento");
+
+    if (title)
+      setEventsData([
+        ...eventsData,
+        {
+          start,
+          end,
+          title
+        }
+        
+      ]);
+  };
   return (
     <div className="App">
         <FullCalendar
+       
         plugins={[ dayGridPlugin,timegridPlugin ,iterationPluging]}
         initialView="dayGridMonth"
-        dateClick={handleAddEvent}
-        
-        event={allEvents}
-        
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        }}
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        dayMaxEvents={true}
+        events={eventsData}
+        eventClick={handleClick}
+        select={handleSelect}
       />
       
     </div>
   )
+ 
+   
+  
 }
 
 export default App
